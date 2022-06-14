7b99d5c075de0631ecfef40324872754b8a00160
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-06-14 18:01:45 +0200

    Modify main function

--- tests/index.js
+++ tests/index.js
@@ -1,4 +1,4 @@
 import assert from "assert";
-import { add } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
+import { minusOne } from "../build/debug.js";
+assert.strictEqual(minusOne(2), 1);
 console.log("ok");
