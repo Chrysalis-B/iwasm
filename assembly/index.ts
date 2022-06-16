206d975aa3cf1499a8b1f510d20fa7c3b8acea35
Heini Ahokas <heini.ahokas@klab-berlin.com>
2022-06-16 20:14:24 +0200

    Implement fizzbuzz in AssemblyScript

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,3 +1,20 @@
 export function minusOne(n: i32): i32 {
-  return n - 1;
+  const result = n - 1;
+  console.log(`${n} minus 1 is ${result}`);
+  return result;
+}
+
+export function fizzbuzz(n: i32): string {
+  let text = ""
+  if (n % 15 === 0) {
+    text = "fizzbuzz";
+  }
+  if (n % 3 === 0) {
+    text = "fizz";
+  }
+  if (n %  5 === 0) {
+    text = "buzz";
+  }
+  console.log(`fizzbuzz returned: ${text}`)
+  return text;
 }
